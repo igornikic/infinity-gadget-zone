c0e9862a00784d788d67c966d37c3fe3caa03540
Igor Nikic <igor.nikic.dev@gmail.com>
2023-07-14 15:43:20 +0200

    Add catchAsyncErrors errorHandler and errors

---
+++ backend/middlewares/errors.js
@@ -0,0 +1,40 @@
+import ErrorHandler from "../utils/errorHandler.js";
+
+export default (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    res.status(err.statusCode).json({
+      success: false,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    let err = { ...err };
+
+    err.message = err.message || "Internal Server Error";
+
+    // Handling Mongoose Object ID Error
+    if (err.name === "CastError") {
+      const message = `Resource not found. Invalid: ${err.path}`;
+      err = new ErrorHandler(message, 400);
+    }
+
+    // Handling Mongoose Validation Error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => {
+        value.message;
+      });
+      err = new ErrorHandler(message, 400);
+    }
+
+    // Handling Mongoose duplicate key errors
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+      err = new ErrorHandler(message, 400);
+    }
+  }
+};
